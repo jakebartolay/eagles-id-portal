abfdc94fbb537aa07d0215025be2063643357abf
Jake Bartolay <jakebartolay147@gmail.com>
2026-02-25 18:19:36 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('https://www.tfoepe-inc.com.ph/laravel-testing/index', function () {
     return view('welcome');
 });
